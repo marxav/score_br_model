2f2c1972eab2087793cabfb2577d3bf6cc858f0a
marxav <xavier.marjou@gmail.com>
2024-07-25 22:30:11 +0200

    update model file at startup

--- usage.py
+++ usage.py
@@ -1,4 +1,6 @@
+import os
 import json
+import requests
 
 def get_price(config, model, input_tokens=0, output_tokens=0):
     # read the dictionary containing the price of the models
@@ -31,3 +33,67 @@ def get_price(config, model, input_tokens=0, output_tokens=0):
     price = input_cost_per_token * input_tokens + output_cost_per_token * output_tokens
  
     return price
+
+
+def update_model_price_list(verbose=False):
+
+    try:
+        # get all directories in './env/lib'
+        directories = [x[0] for x in os.walk('./env/lib')]
+
+        # get the sub-directories name starting with 'python' in './env/lib'
+        # limit the search to the direct sub-directories of './env/lib'
+        python_dirs = [d for d in directories if 'python' in d and len(d.split('/')) == 4]
+        
+        # select the latest version of python in python_dirs
+        python_dir = sorted(python_dirs)[-1]
+
+        
+        site_pack_dir = os.path.join(python_dir, 'site-packages')
+        
+        # check that python_dir contains an litellm folder
+        if 'litellm' not in os.listdir(site_pack_dir):
+            print('ERROR: litellm folder not found in', site_pack_dir)
+            exit(-1)
+        else:
+            litellm_dir = os.path.join(site_pack_dir, 'litellm')
+
+        local_filename = 'model_prices_and_context_window_backup.json'
+        llm_file = os.path.join(litellm_dir, local_filename)
+        
+        if verbose:
+            print('python_dirs:', python_dirs)
+            print('python_dir:', python_dir)
+            print('site_pack_dir:', site_pack_dir)
+            print('litellm_dir:', litellm_dir)
+            print('llm_file:', llm_file)
+
+        # download the latest version thee litellm file descring llms, ie file
+        # https://raw.githubusercontent.com/BerriAI/litellm/main/model_prices_and_context_window.json
+        # and save it in the env/lib/python3.8/site-packages/litellm folder
+        url = 'https://raw.githubusercontent.com/BerriAI/litellm/main/model_prices_and_context_window.json'
+        r = requests.get(url)
+        # read byte content of the file
+        new_content = r.content
+
+        # check that litellm folder contains a file named model_prices_and_context_window.json
+        if local_filename in os.listdir(litellm_dir):
+            # read the content of the local file model_prices_and_context_window.json
+            old_content = open(llm_file, 'rb').read()
+        else:
+            print('WARNING: model_prices_and_context_window.json not found in', litellm_dir)
+            old_content = b''
+
+        if verbose: 
+            print('old_content:', old_content[0:200])
+            print('new_content:', new_content[0:200])
+        
+        if old_content != new_content:
+            with open(llm_file, 'wb+') as f:
+                f.write(new_content)
+            print('model price file updated')
+        else:
+            print('model price file already up-to-date')
+    except Exception as e:
+        print('WARNING: error while updating model price file:', e)
+    return
